c8247fcdc6b13499db99c2b74b47fa7b0aa4b99f
Yulia Bezsmertna <yuliabezsmertna80@gmail.com>
2025-10-20 22:09:22 +0300

    correct

--- src/models/session.js
+++ src/models/session.js
@@ -1,31 +1,33 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
-export const sessionSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User'
+const sessionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    accessToken: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+    },
+    accessTokenValidUntil: {
+      type: Date,
+      required: true,
+    },
+    refreshTokenValidUntil: {
+      type: Date,
+      required: true,
+    },
   },
-  accessToken: {
-    type: String,
-    required: true
-  },
-  refreshToken: {
-    type: String,
-    required: true
-  },
-  accessTokenValidUntil: {
-    type: Date,
-    required: true
-  },
-  refreshTokenValidUntil: {
-    type: Date,
-    required: true
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
+);
 
-const Session = mongoose.model('Session', sessionSchema);
+export const Session = mongoose.model('Session', sessionSchema);
 
-module.exports = Session;
